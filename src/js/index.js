859034bb71c86b272f2b6a85f24a8d3bd970b0c1
devdatzo <devdatzo@gmail.com>
2022-05-25 17:37:06 +0700

    Update

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,6 @@
 const humberger = document.querySelector(".humberger");
 const navMenu = document.querySelector(".nav-menu");
+
 humberger.addEventListener("click", ()=> {
   humberger.classList.toggle("active");
   navMenu.classList.toggle("active");
@@ -8,3 +9,12 @@ document.querySelectorAll(".nav-link").forEach(n=>n.addEventListener("click",()=
   humberger.classList.remove("active");
   navMenu.classList.remove("active");
 }))
+
+const activePage = window.location.pathname;
+const navLink = document.querySelectorAll(".nav-link").forEach(link =>{
+   if(link.href.includes(`${activePage}`)){
+      link.classList.add("actives")
+      console.log(`${activePage}`)
+   }
+   console.log(link)
+})
