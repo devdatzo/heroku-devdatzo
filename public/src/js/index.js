088bd400963727bf236766ba64a7318a99b6aac4
devdatzo <devdatzo@gmail.com>
2022-05-26 06:53:45 +0700

    Update JS

--- public/src/js/index.js
+++ public/src/js/index.js
@@ -11,6 +11,10 @@ document.querySelectorAll(".nav-link").forEach(n=>n.addEventListener("click",()=
 }))
 
 const activePage = window.location.pathname;
+const lplink = document.querySelector(".nav-link")
+if (activePage=='/') {
+  lplink.classList.add("actives") 
+}
 const navLink = document.querySelectorAll(".nav-link").forEach(link =>{
    if(link.href.includes(`${activePage}`)&&activePage!=="/"){
       link.classList.add("actives")
